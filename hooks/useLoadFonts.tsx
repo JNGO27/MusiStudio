21fb900557ee85d0f2480ec0b5b1e3f7f82bfbf7
Joseph Ochoa <josephochoa91@gmail.com>
2023-03-02 10:33:54 -0800

    Add needed package for expo + hook cleanup

---
+++ hooks/useLoadFonts.tsx
@@ -0,0 +1,44 @@
+import {
+  useFonts,
+  Figtree_300Light,
+  Figtree_300Light_Italic,
+  Figtree_400Regular,
+  Figtree_400Regular_Italic,
+  Figtree_500Medium_Italic,
+  Figtree_500Medium,
+  Figtree_600SemiBold,
+  Figtree_600SemiBold_Italic,
+  Figtree_700Bold,
+  Figtree_700Bold_Italic,
+  Figtree_800ExtraBold,
+  Figtree_800ExtraBold_Italic,
+  Figtree_900Black,
+  Figtree_900Black_Italic,
+} from "@expo-google-fonts/figtree";
+
+const useLoadFonts = () => {
+  const [fontsLoaded] = useFonts({
+    Figtree_300Light,
+    Figtree_300Light_Italic,
+    Figtree_400Regular,
+    Figtree_400Regular_Italic,
+    Figtree_500Medium_Italic,
+    Figtree_500Medium,
+    Figtree_600SemiBold,
+    Figtree_600SemiBold_Italic,
+    Figtree_700Bold,
+    Figtree_700Bold_Italic,
+    Figtree_800ExtraBold,
+    Figtree_800ExtraBold_Italic,
+    Figtree_900Black,
+    Figtree_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return null;
+};
+
+export default useLoadFonts;
