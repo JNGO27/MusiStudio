c760d3aa19381907d02b08474e44c5b82ca5223c
Joseph Ochoa <josephochoa91@gmail.com>
2023-03-02 18:51:51 -0800

    Add basic styles to Auth Card

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -15,7 +15,7 @@ const Auth = () => {
   const [password, setPassword] = useState("");
   const [, setLoading] = useState(false);
 
-  async function signInWithEmail() {
+  const signInWithEmail = async () => {
     setLoading(true);
     const { error } = await supabaseConfig.auth.signInWithPassword({
       email,
@@ -24,9 +24,9 @@ const Auth = () => {
 
     if (error) Alert.alert(error.message);
     setLoading(false);
-  }
+  };
 
-  async function signUpWithEmail() {
+  const signUpWithEmail = async () => {
     setLoading(true);
     const { error } = await supabaseConfig.auth.signUp({
       email,
@@ -35,27 +35,31 @@ const Auth = () => {
 
     if (error) Alert.alert(error.message);
     setLoading(false);
-  }
+  };
 
   return (
-    <SafeAreaView>
-      <TextInput
-        onChangeText={setEmail}
-        value={email}
-        placeholder="Email Address"
-      />
-      <TextInput
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Password"
-      />
-      <View>
-        <TouchableOpacity onPress={() => signInWithEmail}>
-          <Text>Sign in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => signUpWithEmail}>
-          <Text>Sign up</Text>
-        </TouchableOpacity>
+    <SafeAreaView className="w-full h-full flex justify-center items-center">
+      <View className="w-80 h-2/4 bg-slate-100 flex justify-center gap-4 py-4 px-12 shadow-md shadow-black">
+        <TextInput
+          className="flex bg-slate-300"
+          onChangeText={setEmail}
+          value={email}
+          placeholder="Email Address"
+        />
+        <TextInput
+          className="flex bg-slate-300"
+          onChangeText={setPassword}
+          value={password}
+          placeholder="Password"
+        />
+        <View className="flex flex-row gap-5">
+          <TouchableOpacity onPress={signInWithEmail}>
+            <Text>Sign in</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={signUpWithEmail}>
+            <Text>Sign up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
