abdf60679f7ec3b401ad7e03150b7fe39e99c107
Joseph Ochoa <josephochoa91@gmail.com>
2023-03-10 14:42:30 -0800

    Add purple gradient

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,8 +1,34 @@
-const SPACING = {
+export const SPACING = {
   multiple: 2,
 };
 
-// eslint-disable-next-line import/prefer-default-export
+export const COLORS = {
+  gradients: {
+    purpleGradient: {
+      colors: [
+        "hsl(243 85% 18%)",
+        "hsl(243 79% 21%)",
+        "hsl(243 74% 25%)",
+        "hsl(242 71% 29%)",
+        "hsl(242 66% 33%)",
+        "hsl(242 59% 37%)",
+        "hsl(242 54% 41%)",
+        "hsl(242 49% 45%)",
+        "hsl(242 47% 47%)",
+        "hsl(242 45% 48%)",
+        "hsl(242 43% 50%)",
+        "hsl(242 43% 51%)",
+      ],
+      locations: [
+        0.01, 0.07, 0.1, 0.22, 0.3, 0.38, 0.47, 0.56, 0.65, 0.75, 0.87, 1,
+      ],
+      start: { x: 1, y: 1.15 },
+      end: { x: 0, y: 0 },
+    },
+  },
+};
+
 export default {
   spacing: SPACING,
+  colors: COLORS,
 };
--- src/screens/Auth/Auth.tsx
+++ src/screens/Auth/Auth.tsx
@@ -1,14 +1,26 @@
-import { View } from "react-native";
-
 import { AuthOption, OAuthOption } from "@src/components";
+import { LinearGradient } from "expo-linear-gradient";
+import globalStyles from "@src/globalStyles";
 import styles from "./styles";
+
+const {
+  colors: {
+    gradients: { purpleGradient },
+  },
+} = globalStyles;
 
 const Auth = () => {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      style={styles.container}
+      colors={purpleGradient.colors}
+      locations={purpleGradient.locations}
+      start={purpleGradient.start}
+      end={purpleGradient.end}
+    >
       <AuthOption authOption="SignIn" />
       <OAuthOption provider="google" />
-    </View>
+    </LinearGradient>
   );
 };
 
