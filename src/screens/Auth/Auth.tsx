d7888cc8ef224d6ea1a12bc74c6d4da7ae68284a
Joseph Ochoa <josephochoa91@gmail.com>
2023-03-11 18:13:49 -0800

    Add decorations to Auth Card

--- src/screens/Auth/Auth.tsx
+++ src/screens/Auth/Auth.tsx
@@ -1,8 +1,12 @@
 import { View } from "react-native";
 import { AuthOption, OAuthOption } from "@src/components";
 import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "expo-image";
+
 import globalStyles from "@src/globalStyles";
 import styles from "./styles";
+import People from "./people.png";
+import AccountPerson from "./AccountPerson";
 
 const {
   colors: {
@@ -19,7 +23,16 @@ const Auth = () => {
       start={purpleGradient.start}
       end={purpleGradient.end}
     >
+      <Image source={People} contentFit="contain" style={styles.image} />
       <View style={styles.card}>
+        <View style={styles.decorationsContainer}>
+          <View style={styles.decorationsContainerInner}>
+            <View style={styles.decorationCircle} />
+            <View style={styles.decorationBox} />
+            <View style={styles.decorationCircle} />
+          </View>
+          <AccountPerson style={styles.accountPerson} />
+        </View>
         <OAuthOption provider="google" />
         <AuthOption authOption="SignIn" />
       </View>
