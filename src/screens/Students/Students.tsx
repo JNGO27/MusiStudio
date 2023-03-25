abc04e5557e5c36e8c218311edd64975c25c4bba
Joseph Ochoa <josephochoa91@gmail.com>
2023-03-25 16:08:30 -0700

    Render AllStudentData in single container

--- src/screens/Students/Students.tsx
+++ src/screens/Students/Students.tsx
@@ -3,23 +3,27 @@ import { SafeAreaView, FlatList } from "react-native";
 import uuid from "react-native-uuid";
 
 import { supabaseConfig } from "@src/lib/supabaseConfig";
-import type { StudentCardType } from "@src/types";
-import { StudentCard, DataCardsContainer } from "@src/components";
+import type { StudentAndFamily } from "@src/types";
+import { StudentCard, FamilyCard, DataCardsContainer } from "@src/components";
 import useResponsiveness from "@src/hooks/useResponsiveness";
 import createStyleSheet from "./styles";
 
 const Students = () => {
-  const [students, setStudents] = useState<StudentCardType[]>([]);
+  const [allData, setAllData] = useState<StudentAndFamily[]>([]);
   const [horizontalScale] = useResponsiveness();
   const styles = createStyleSheet(horizontalScale);
 
   useEffect(() => {
     async function getData() {
-      const { data: studentsData } = await supabaseConfig
+      const { data: allStudentsData } = await supabaseConfig
         .from("Students")
-        .select("id, first_name, last_name, phone_number, email_address");
+        .select(
+          "id, first_name, last_name, phone_number, email_address, associated_family (parent_guardian_first_name_1, parent_guardian_last_name_1, phone_number, email_address)",
+        );
 
-      setStudents(studentsData);
+      if (allStudentsData) {
+        setAllData(allStudentsData);
+      }
     }
 
     getData();
@@ -30,7 +34,7 @@ const Students = () => {
       <FlatList
         style={styles.cardsContainer}
         contentContainerStyle={styles.cardsContainerFlex}
-        data={students}
+        data={allData}
         renderItem={({ item }) => (
           <DataCardsContainer
             allStudentData={[
@@ -41,19 +45,12 @@ const Students = () => {
                 phone_number={item.phone_number}
                 email_address={item.email_address}
               />,
-              <StudentCard
+              <FamilyCard
                 key={item.id}
-                first_name={item.first_name}
-                last_name={item.last_name}
-                phone_number={item.phone_number}
-                email_address={item.email_address}
-              />,
-              <StudentCard
-                key={item.id}
-                first_name={item.first_name}
-                last_name={item.last_name}
-                phone_number={item.phone_number}
-                email_address={item.email_address}
+                first_name={item.associated_family.parent_guardian_first_name_1}
+                last_name={item.associated_family.parent_guardian_last_name_1}
+                phone_number={item.associated_family.phone_number}
+                email_address={item.associated_family.email_address}
               />,
             ]}
           />
--- src/types/cardTypes.ts
+++ src/types/cardTypes.ts
@@ -1,14 +1,35 @@
 export type Student = {
+  id: number;
   associated_family: number;
   first_name: string;
-  id: number;
   last_name: string;
   email_address: string;
   phone_number: string;
 };
 
-type StudentOmit = "associated_family" | "id";
+type StudentOmit = "id" | "associated_family";
 
 export type StudentCardType = Omit<Student, StudentOmit> & {
   id?: number;
+};
+
+export type Family = {
+  id: number;
+  parent_guardian_first_name_1: string;
+  parent_guardian_last_name_1: string;
+  parent_guardian_first_name_2: string;
+  parent_guardian_last_name_2: string;
+  email_address: string;
+  phone_number: string;
+};
+
+type FamilyOmit =
+  | "id"
+  | "parent_guardian_first_name_2"
+  | "parent_guardian_last_name_2";
+
+export type FamilyCardType = Omit<Family, FamilyOmit>;
+
+export type StudentAndFamily = StudentCardType & {
+  associated_family: FamilyCardType;
 };
