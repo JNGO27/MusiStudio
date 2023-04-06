999955b6be73a0d952990386a4e8c4e1ac08429f
Joseph Ochoa <josephochoa91@gmail.com>
2023-04-05 17:00:05 -0700

    Move future features to legacy folder

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,8 +1,4 @@
 export * from "./Students";
 
 export { default as Auth } from "./Auth/Auth";
-export { default as SignUp } from "./SignUp/SignUp";
-export { default as SignIn } from "./SignIn/SignIn";
-export { default as ForgotPassword } from "./ForgotPassword/ForgotPassword";
-export { default as ResetForm } from "./ResetForm/ResetForm";
 export { default as EmailOnlyAuth } from "./EmailOnlyAuth/EmailOnlyAuth";
