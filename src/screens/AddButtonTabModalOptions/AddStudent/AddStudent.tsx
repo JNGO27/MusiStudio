0bec61ca7c5b906e308cdf94e9fc871c62575952
Joseph Ochoa <josephochoa91@gmail.com>
2023-04-12 15:37:22 -0700

    Make AddStudent Screen with basic boilerplate + export/imports

---
+++ src/screens/AddButtonTabModalOptions/AddStudent/AddStudent.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AddStudent = () => {
+  return (
+    <View>
+      <Text>AddStudent</Text>
+    </View>
+  );
+};
+
+export default AddStudent;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,5 @@
 export * from "./Students";
 
 export * from "./Auth";
+
+export * from "./AddButtonTabModalOptions";
