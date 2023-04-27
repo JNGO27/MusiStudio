86126f9ad2c6413b6af1bd41c0c070e023b6e319
Joseph Ochoa <josephochoa91@gmail.com>
2023-04-27 12:29:19 -0700

    Add student illustrations for AddStudent Screen

--- src/assets/illustrations/index.ts
+++ src/assets/illustrations/index.ts
@@ -1,2 +1,4 @@
 export { default as PeopleMusic } from "./people-music.png";
 export { default as PeoplePhone } from "./people.png";
+export { default as StudentIllustration } from "./studentIllustration.png";
+export { default as PianoStudentIllustration } from "./pianoStudentIllustration.png";
