cce33e97e2794fcc14216a617178f4a4c00780a3
Joseph Ochoa <josephochoa91@gmail.com>
2023-04-28 10:39:33 -0700

    Add blacks and greens to globalStyles

--- src/components/CheckboxCard/styles.ts
+++ src/components/CheckboxCard/styles.ts
@@ -4,7 +4,10 @@ import { DirectionalScale, CalculatedScale } from "@src/types";
 
 import globalStyles from "@src/globalStyles";
 
-const { spacing } = globalStyles;
+const {
+  spacing,
+  colors: { grays, greens },
+} = globalStyles;
 
 export default (
   horizontalScale: DirectionalScale,
@@ -15,11 +18,13 @@ export default (
   return StyleSheet.create({
     checkboxCard: {
       display: "flex",
-      width: horizontalScale(spacing.multipleXL * 12),
+      width: horizontalScale(spacing.multipleXL * 13),
       height: verticalScale(spacing.multipleXL * 10),
       borderRadius: spacing.multipleReg * 4,
-      opacity: 1,
-      backgroundColor: isChosen ? "lightgreen" : "white",
+      opacity: 2,
+      backgroundColor: isChosen ? greens.green50 : "white",
+      borderWidth: spacing.multipleXS,
+      borderColor: isChosen ? greens.green500 : grays.gray300,
     },
   });
 };
