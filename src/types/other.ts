e287a9b72e4975e835482adac5c3064cda2382e9
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-01 11:34:26 -0700

    Add type for StyleSheet Object

--- src/types/other.ts
+++ src/types/other.ts
@@ -1,5 +1,9 @@
-import type { GestureResponderEvent } from "react-native";
+import type { GestureResponderEvent, ViewStyle, TextStyle } from "react-native";
 
 export type FormikSubmit = (
   values: GestureResponderEvent | React.FormEvent<HTMLFormElement> | undefined,
 ) => void;
+
+export type StyleSheetProps = {
+  [key: string]: ViewStyle | TextStyle;
+};
