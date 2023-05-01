f064ecda3383f44b0dbfc2e8167ddfee3f1d69d2
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-01 11:31:46 -0700

    Export all new Form Component helpers

---
+++ src/screens/AddButtonTabModalOptions/AddStudent/FormComponentHelpers/index.ts
@@ -0,0 +1,17 @@
+import HeroSection from "./HeroSection";
+import StudentDetails from "./StudentDetails";
+import StudentFamilyChoice from "./StudentFamilyChoice";
+import FamilyDetails from "./FamilyDetails";
+import LessonDetails from "./LessonDetails";
+import RateDetails from "./RateDetails";
+import Finalization from "./Finalization";
+
+export {
+  HeroSection,
+  StudentDetails,
+  StudentFamilyChoice,
+  FamilyDetails,
+  LessonDetails,
+  RateDetails,
+  Finalization,
+};
