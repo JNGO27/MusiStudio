cc110679d93d73cde80accb0d880f3183558d469
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-12 13:58:11 -0700

    Add custom Header Component with custom styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,10 +3,39 @@ import { StyleSheet } from "react-native";
 
 import { DirectionalScale, CalculatedScale } from "@src/types";
 
+import globalStyles from "@src/globalStyles";
+
+const {
+  spacing,
+  colors: { whites },
+  typography,
+} = globalStyles;
+
 export default (
   horizontalScale: DirectionalScale,
   verticalScale: DirectionalScale,
   moderateScale: CalculatedScale,
 ) => {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    headerContainer: {
+      position: "absolute",
+      top: 0,
+      backgroundColor: whites.white100,
+      width: "100%",
+      height: spacing.multipleXL * 5.5,
+      paddingHorizontal: spacing.multipleXL * 2,
+      borderBottomLeftRadius: spacing.multipleXL * 4,
+      borderBottomRightRadius: spacing.multipleXL * 4,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.5,
+      shadowRadius: 2,
+      elevation: 5,
+    },
+    headerText: { fontFamily: typography.semiBold },
+  });
 };
--- src/navigation/RootNav/RootNav.tsx
+++ src/navigation/RootNav/RootNav.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { TabNavigator, HeaderNav } from "@src/navigation";
-import { AccountNavOption } from "@src/components";
+import { AccountNavOption, Header } from "@src/components";
 
 import type { RootStackParamList } from "@src/types";
 
@@ -14,7 +14,7 @@ const RootNav = () => {
       <RootStack.Navigator
         initialRouteName="TabNavigator"
         screenOptions={{
-          headerRight: AccountNavOption,
+          header: Header,
         }}
       >
         <RootStack.Screen name="TabNavigator" component={TabNavigator} />
