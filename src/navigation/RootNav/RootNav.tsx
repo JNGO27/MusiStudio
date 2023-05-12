04a2924123492057351de9dfaaa1a9670579f10f
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-12 12:04:14 -0700

    Add AccountNavOption and HeaderNav to RootNavigation component

--- src/navigation/RootNav/RootNav.tsx
+++ src/navigation/RootNav/RootNav.tsx
@@ -1,8 +1,8 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { TabNavigator } from "@src/navigation";
-import { Header } from "@src/components";
+import { TabNavigator, HeaderNav } from "@src/navigation";
+import { AccountNavOption } from "@src/components";
 
 import type { RootStackParamList } from "@src/types";
 
@@ -11,12 +11,17 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 const RootNav = () => {
   return (
     <NavigationContainer>
-      <Header />
-      <RootStack.Navigator>
+      <RootStack.Navigator
+        initialRouteName="TabNavigator"
+        screenOptions={{
+          headerRight: AccountNavOption,
+        }}
+      >
+        <RootStack.Screen name="TabNavigator" component={TabNavigator} />
         <RootStack.Screen
+          name="HeaderNav"
           options={{ headerShown: false }}
-          name="TabNavigator"
-          component={TabNavigator}
+          component={HeaderNav}
         />
       </RootStack.Navigator>
     </NavigationContainer>
