a9c9f7ac85d152f0a6955af853ca9e987849a2b5
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-15 15:38:06 -0700

    Add OptionTypes and phoneNumber to CallOrMessageContext

--- src/contexts/CallOrMessageContext.tsx
+++ src/contexts/CallOrMessageContext.tsx
@@ -1,12 +1,20 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext, useState, useMemo } from "react";
 
-import type { ReactNode } from "react";
+import type { Dispatch, SetStateAction, ReactNode } from "react";
 
 import { useNewModalState } from "@src/hooks";
+
+type UseStateSetter<T> = Dispatch<SetStateAction<T>>;
+
+type OptionTypes = "" | "Call" | "Message";
 
 type ModalContextProps = {
   modalVisible: boolean;
   openOrCloseModal: () => void;
+  optionType: OptionTypes;
+  setOptionType: UseStateSetter<OptionTypes>;
+  phoneNumber: string;
+  setPhoneNumber: UseStateSetter<string>;
 };
 
 type Props = {
@@ -17,13 +25,19 @@ const Context = createContext<ModalContextProps>({} as ModalContextProps);
 
 export const CallOrMessageContext = ({ children }: Props) => {
   const [modalVisible, openOrCloseModal] = useNewModalState();
+  const [optionType, setOptionType] = useState<OptionTypes>("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const contextValue = useMemo(
     () => ({
       modalVisible,
       openOrCloseModal,
+      optionType,
+      setOptionType,
+      phoneNumber,
+      setPhoneNumber,
     }),
-    [openOrCloseModal, modalVisible],
+    [modalVisible, openOrCloseModal, optionType, phoneNumber],
   );
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
