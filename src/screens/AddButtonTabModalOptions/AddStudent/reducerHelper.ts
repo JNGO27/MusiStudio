cdbf11cb03df238e6553676dc626badde3238c43
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-16 14:12:49 -0700

    Remove completely unneeded reducer helpers + types

--- src/screens/AddButtonTabModalOptions/AddStudent/reducerHelper.ts
+++ src/screens/AddButtonTabModalOptions/AddStudent/reducerHelper.ts
@@ -3,8 +3,6 @@ import {
   FamilyTypeCheckboxesActions,
   RateCheckboxesState,
   RateCheckboxesActions,
-  RequiredStudentFormValues,
-  RequiredStudentFormValuesActions,
 } from "@src/types";
 
 export const familyTypeInitialState: FamilyTypeCheckboxesState = {
@@ -16,14 +14,6 @@ export const rateInitialState: RateCheckboxesState = {
   PER_HOUR: true,
   PER_LESSON: false,
   PER_MONTH: false,
-};
-
-export const requiredFormValuesInitalState: RequiredStudentFormValues = {
-  first_name_empty: false,
-  last_name_empty: false,
-  family_first_name_empty: false,
-  family_last_name_empty: false,
-  rate_empty: false,
 };
 
 export function familyTypeReducer(
@@ -71,40 +61,5 @@ export function rateReducer(
       };
     default:
       throw new Error("Must return a valid checkbox state object");
-  }
-}
-
-export function requiredFormValuesReducer(
-  formValues: RequiredStudentFormValues,
-  action: RequiredStudentFormValuesActions,
-): RequiredStudentFormValues {
-  switch (action.type) {
-    case "Student First Name Required":
-      return {
-        ...formValues,
-        first_name_empty: true,
-      };
-    case "Student Last Name Required":
-      return {
-        ...formValues,
-        last_name_empty: true,
-      };
-    case "Parent First Name Required":
-      return {
-        ...formValues,
-        family_first_name_empty: true,
-      };
-    case "Parent Last Name Required":
-      return {
-        ...formValues,
-        family_last_name_empty: true,
-      };
-    case "Rate Required":
-      return {
-        ...formValues,
-        rate_empty: true,
-      };
-    default:
-      throw new Error("Must return a required student form values object.");
   }
 }
