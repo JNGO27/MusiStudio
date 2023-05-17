c4e520383e42674e000f4fb4d3da02070808c67a
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-16 19:10:27 -0700

    Add styles to TimedMessageComponent

--- src/components/TimedStatusMessage/styles.ts
+++ src/components/TimedStatusMessage/styles.ts
@@ -3,10 +3,66 @@ import { StyleSheet } from "react-native";
 
 import { DirectionalScale, CalculatedScale } from "@src/types";
 
+import globalStyles from "@src/globalStyles";
+
+const {
+  spacing,
+  colors: { reds, greens, blacks },
+  typography,
+} = globalStyles;
+
 export default (
   horizontalScale: DirectionalScale,
   verticalScale: DirectionalScale,
   moderateScale: CalculatedScale,
 ) => {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    messageSuccessContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      bottom: 125,
+      width: "80%",
+      height: verticalScale(spacing.multipleXL * 4.5),
+      paddingHorizontal: spacing.multipleXL,
+      borderRadius: spacing.multipleXL * 4,
+      borderColor: greens.green700,
+      borderWidth: spacing.multipleXS,
+      backgroundColor: greens.green50,
+      shadowColor: greens.green700,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 2,
+      shadowRadius: 2,
+      elevation: 12,
+    },
+    messageErrorContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      bottom: 125,
+      width: "90%",
+      height: verticalScale(spacing.multipleXL * 5),
+      paddingHorizontal: spacing.multipleXL,
+      borderRadius: spacing.multipleXL * 4,
+      borderColor: reds.red700,
+      borderWidth: spacing.multipleXS,
+      backgroundColor: reds.red50,
+      shadowColor: reds.red700,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 2,
+      shadowRadius: 2,
+      elevation: 12,
+    },
+    messageText: {
+      position: "relative",
+      top: spacing.multipleXS,
+      lineHeight: spacing.multipleReg * 3,
+      textAlign: "center",
+      fontFamily: typography.semiBold,
+      fontSize: moderateScale(14),
+      color: blacks.black,
+    },
+  });
 };
