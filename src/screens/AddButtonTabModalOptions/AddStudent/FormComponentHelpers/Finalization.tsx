879964214bd469ae1dda4ff95f5a266be9a2952b
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-18 16:46:02 -0700

    Add custom formSubmission function to handle conditional styles and submission

--- src/screens/AddButtonTabModalOptions/AddStudent/FormComponentHelpers/Finalization.tsx
+++ src/screens/AddButtonTabModalOptions/AddStudent/FormComponentHelpers/Finalization.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { View, Text, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useFormikContext } from "formik";
 
-import type { FormikHandlers } from "formik";
+import type { Dispatch, SetStateAction } from "react";
 
-import type { StyleSheetProps, FormikSubmit } from "@src/types";
+import type { StudentFormValues } from "@src/types";
+
+import { useAddStudentFormContext } from "@src/contexts/AddStudentFormContext";
 
 import globalStyles from "@src/globalStyles";
 
@@ -14,11 +18,38 @@ const {
 } = globalStyles;
 
 type Props = {
-  handleSubmit: FormikHandlers["handleSubmit"];
-  styles: StyleSheetProps;
+  setTimedErrorOccurred: Dispatch<SetStateAction<boolean>>;
 };
 
-const Finalization = ({ handleSubmit, styles }: Props) => {
+const Finalization = ({ setTimedErrorOccurred }: Props) => {
+  const { validateForm, setTouched } = useFormikContext();
+  const { handleSubmit, values, styles } = useAddStudentFormContext();
+
+  const handleErrorStyles = (formValues: StudentFormValues) => {
+    const { first_name, last_name, family_first_name, family_last_name, rate } =
+      formValues;
+
+    setTouched({
+      first_name: first_name.length === 0,
+      last_name: last_name.length === 0,
+      family_first_name: family_first_name.length === 0,
+      family_last_name: family_last_name.length === 0,
+      rate: rate.length === 0,
+    });
+  };
+
+  const handleOnFormSubmit = async () => {
+    const errors = await validateForm();
+    handleErrorStyles(values);
+    const hasErrors = Object.keys(errors).length > 0;
+
+    if (hasErrors) {
+      setTimedErrorOccurred(true);
+    } else {
+      handleSubmit();
+    }
+  };
+
   return (
     <View style={styles.saveOrCancelContainer}>
       <LinearGradient
@@ -30,7 +61,7 @@ const Finalization = ({ handleSubmit, styles }: Props) => {
       >
         <TouchableOpacity
           style={styles.saveButton}
-          onPress={handleSubmit as FormikSubmit}
+          onPress={handleOnFormSubmit}
         >
           <Text style={styles.saveButtonText}>Save</Text>
         </TouchableOpacity>
