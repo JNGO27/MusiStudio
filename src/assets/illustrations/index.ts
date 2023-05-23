8d8c9710b5aa2484a480134afbc415f4505a9520
Joseph Ochoa <josephochoa91@gmail.com>
2023-05-23 15:19:23 -0700

    Add Student Avatar Images

--- src/assets/illustrations/index.ts
+++ src/assets/illustrations/index.ts
@@ -2,3 +2,7 @@ export { default as PeopleMusic } from "./people-music.png";
 export { default as PeoplePhone } from "./people.png";
 export { default as StudentIllustration } from "./studentIllustration.png";
 export { default as PianoStudentIllustration } from "./pianoStudentIllustration.png";
+export { default as StudentChildMale } from "./StudentChildMale.png";
+export { default as StudentChildMaleTwo } from "./StudentChildMaleTwo.png";
+export { default as StudentChildFemale } from "./StudentChildFemale.png";
+export { default as StudentChildFemaleTwo } from "./StudentChildFemaleTwo.png";
