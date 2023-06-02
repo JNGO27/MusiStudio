9abaad5854ee1b3c52aec10430250dd53693770b
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-02 15:18:18 -0700

    Add props and logic to WarningModal

--- src/components/WarningModal/WarningModal.tsx
+++ src/components/WarningModal/WarningModal.tsx
@@ -1,28 +1,82 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Text, View } from "react-native";
+import {
+  Modal,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+  Text,
+} from "react-native";
+import { Image } from "expo-image";
 
-import type { Dispatch, SetStateAction } from "react";
+import type { FunctionType } from "@src/types";
 
 import useResponsiveness from "@src/hooks/useResponsiveness";
+
+import { WarningIcon } from "@src/assets/icons";
+
 import createStyleSheet from "./styles";
 
 type Props = {
-  isVisible: boolean;
-  setModalVisible: Dispatch<SetStateAction<boolean>>;
+  dispatchWarningAction: FunctionType;
+  warningHeaderText: string;
+  warningBodyText: string;
+  warningActionText: string;
+  modalVisible: boolean;
+  openOrCloseModal: () => void;
 };
 
-const WarningModal = ({ isVisible, setModalVisible }: Props) => {
+const WarningModal = ({
+  dispatchWarningAction,
+  warningHeaderText,
+  warningBodyText,
+  warningActionText,
+  modalVisible,
+  openOrCloseModal,
+}: Props) => {
   const [horizontalScale, verticalScale, moderateScale] = useResponsiveness();
   const styles = createStyleSheet(
     horizontalScale,
     verticalScale,
     moderateScale,
+    modalVisible,
   );
 
   return (
-    <View>
-      <Text>WarningModal</Text>
-    </View>
+    <Modal
+      style={styles.modalContainer}
+      visible={modalVisible}
+      onRequestClose={openOrCloseModal}
+      animationType="slide"
+      transparent
+    >
+      <TouchableWithoutFeedback onPress={openOrCloseModal}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalCard}>
+            <View style={styles.innerContainer}>
+              <Image style={styles.icon} source={WarningIcon} />
+              <View style={styles.optionsContainer}>
+                <Text style={styles.headerText}>{warningHeaderText}</Text>
+                <Text style={styles.bodyText}>{warningBodyText}</Text>
+              </View>
+            </View>
+            <View style={styles.buttonsContainer}>
+              <TouchableOpacity
+                style={styles.buttonWarningAction}
+                onPress={dispatchWarningAction}
+              >
+                <Text style={styles.buttonText}>{warningActionText}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.buttonCancel}
+                onPress={openOrCloseModal}
+              >
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
   );
 };
 
