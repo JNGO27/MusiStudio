dc8ffd9b2e0275a7aab9969eb7377485fad38c24
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-05 10:30:07 -0700

    Use custom icon for BackButtonCustom and use useNavigator to go back

--- src/components/BackButtonCustom/BackButtonCustom.tsx
+++ src/components/BackButtonCustom/BackButtonCustom.tsx
@@ -1,21 +1,36 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import type { NavigationProp } from "@react-navigation/native";
+
+import type { AllNavParamLists, AllNavOptions } from "@src/types";
 
 import useResponsiveness from "@src/hooks/useResponsiveness";
+
+import { BackIcon } from "@src/assets/icons";
+
 import createStyleSheet from "./styles";
 
+type BackOptions = NavigationProp<AllNavParamLists>;
+
 const BackButtonCustom = () => {
+  const navigator = useNavigation<BackOptions>();
+
   const [horizontalScale, verticalScale, moderateScale] = useResponsiveness();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const styles = createStyleSheet(
     horizontalScale,
     verticalScale,
     moderateScale,
   );
 
+  const handleNavigation = () => {
+    navigator.goBack();
+  };
+
   return (
-    <View>
-      <Text>BackButtonCustom</Text>
-    </View>
+    <TouchableOpacity style={styles.iconContainer} onPress={handleNavigation}>
+      <BackIcon style={styles.icon} />
+    </TouchableOpacity>
   );
 };
 
