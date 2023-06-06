c3bd80606e7025ecacda667447191caf76e6722d
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-06 14:41:07 -0700

    Add Dashboard Card for Students with static count

--- src/components/DashboardCard/DashboardCard.tsx
+++ src/components/DashboardCard/DashboardCard.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableHighlight } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import useResponsiveness from "@src/hooks/useResponsiveness";
+
+import { UserStudent } from "@src/assets/icons";
+
+import globalStyles from "@src/globalStyles";
 import createStyleSheet from "./styles";
 
-const DashboardCard = () => {
+const {
+  colors: {
+    blacks: { blackTransparent },
+    gradients: { purpleGradient },
+  },
+} = globalStyles;
+
+type Props = {
+  type: "Students";
+};
+
+const DashboardCard = ({ type }: Props) => {
   const [horizontalScale, verticalScale, moderateScale] = useResponsiveness();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const styles = createStyleSheet(
     horizontalScale,
     verticalScale,
@@ -13,8 +28,26 @@ const DashboardCard = () => {
   );
 
   return (
-    <View>
-      <Text>DashboardCard</Text>
+    <View style={styles.touchableBackground}>
+      {type === "Students" && (
+        <TouchableHighlight
+          style={styles.touchableContainer}
+          underlayColor={blackTransparent}
+        >
+          <LinearGradient
+            style={styles.dashboardCardContainer}
+            colors={purpleGradient.colors}
+            locations={purpleGradient.locations}
+            start={purpleGradient.start}
+            end={purpleGradient.end}
+          >
+            <View style={styles.circleDecoration}>
+              <UserStudent style={styles.studentsIcon} />
+            </View>
+            <Text style={styles.studentsText}>7 / 50 Students</Text>
+          </LinearGradient>
+        </TouchableHighlight>
+      )}
     </View>
   );
 };
