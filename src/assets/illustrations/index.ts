b66978519addd4c78c66dc9bbba909495a5f2da6
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-06 13:33:10 -0700

    Remove 2 illustrations due to crediting issues

--- src/assets/illustrations/index.ts
+++ src/assets/illustrations/index.ts
@@ -1,5 +1,3 @@
-export { default as PeopleMusic } from "./people-music.png";
-export { default as PeoplePhone } from "./people.png";
 export { default as StudentIllustration } from "./studentIllustration.png";
 export { default as PianoStudentIllustration } from "./pianoStudentIllustration.png";
 export { default as StudentChildMale } from "./StudentChildMale.png";
