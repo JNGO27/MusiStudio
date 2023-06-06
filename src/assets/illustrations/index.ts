88ec517180863aad411439ce0c3d207e57a6f1ee
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-06 13:24:52 -0700

    Add TwoStudentsEnjoyingMusicIllustration

--- src/assets/illustrations/index.ts
+++ src/assets/illustrations/index.ts
@@ -7,3 +7,4 @@ export { default as StudentChildMaleTwo } from "./StudentChildMaleTwo.png";
 export { default as StudentChildFemale } from "./StudentChildFemale.png";
 export { default as StudentChildFemaleTwo } from "./StudentChildFemaleTwo.png";
 export { default as StudentPlayingStudentDancing } from "./StudentPlayingStudentDancing.png";
+export { default as TwoStudentsEnjoyingMusic } from "./TwoStudentsEnjoyingMusic.png";
