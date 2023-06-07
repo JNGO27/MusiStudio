dc0daa8fd3bd77d3c6bd2ffb4bce0690ec57a61a
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-07 14:13:28 -0700

    Remove imports

--- src/components/FormComponentHelpersEditFamily/index.ts
+++ src/components/FormComponentHelpersEditFamily/index.ts
@@ -1,23 +1,6 @@
 import HeroSection from "./HeroSection";
-import StudentDetails from "./StudentDetails";
-import AdditionalStudentDetails from "./AdditionalStudentDetails";
-import StudentFamilyChoice from "./StudentFamilyChoice";
-import ExistingFamilyChoice from "./ExistingFamilyChoice";
-import ExistingFamilyOptions from "./ExistingFamilyOptions";
+import AdditionalFamilyDetails from "./AdditionalFamilyDetails";
 import FamilyDetails from "./FamilyDetails";
-import LessonDetails from "./LessonDetails";
-import RateDetails from "./RateDetails";
 import Finalization from "./Finalization";
 
-export {
-  HeroSection,
-  StudentDetails,
-  AdditionalStudentDetails,
-  StudentFamilyChoice,
-  ExistingFamilyChoice,
-  ExistingFamilyOptions,
-  FamilyDetails,
-  LessonDetails,
-  RateDetails,
-  Finalization,
-};
+export { HeroSection, AdditionalFamilyDetails, FamilyDetails, Finalization };
