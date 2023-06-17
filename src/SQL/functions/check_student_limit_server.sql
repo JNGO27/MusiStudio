52aa8719e742c0f6b76174b1f09b68a4ba9102dd
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-17 14:45:02 -0700

    Add check_student_limit client & server to check if current student count is greater than or equal to max_students

---
+++ src/SQL/functions/check_student_limit_server.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION check_student_limit_server() RETURNS BOOLEAN AS $$
+BEGIN
+ RETURN ((
+   (SELECT COUNT(*) FROM "Students" WHERE user_id = auth.uid())
+   > 
+   (SELECT max_students FROM "Tiers" WHERE id = (SELECT tier FROM "Profiles" WHERE user_id = auth.uid()))
+ ));
+END;
+$$ LANGUAGE plpgsql;
