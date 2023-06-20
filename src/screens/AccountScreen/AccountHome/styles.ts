7519bc6879b96c31bd44c7682a31f27846bd2b24
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-19 19:22:54 -0700

    Add styles to Log Out button

--- src/screens/AccountScreen/AccountHome/styles.ts
+++ src/screens/AccountScreen/AccountHome/styles.ts
@@ -5,7 +5,7 @@ import { DirectionalScale, CalculatedScale } from "@src/types";
 import globalStyles from "@src/globalStyles";
 
 const {
-  colors: { whites },
+  colors: { whites, purples },
   spacing,
   typography,
 } = globalStyles;
@@ -35,6 +35,29 @@ export default (
       display: "flex",
       gap: spacing.multipleXL * 2,
       width: "100%",
+    },
+    logOutButton: {
+      position: "absolute",
+      bottom: verticalScale(spacing.multipleXL * 6),
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: horizontalScale(spacing.multipleXL * 15),
+      height: verticalScale(spacing.multipleReg * 5),
+      borderColor: purples.purple1000,
+      borderWidth: spacing.multipleXS,
+      borderRadius: spacing.multipleReg * 2.5,
+      backgroundColor: purples.purple100,
+      shadowColor: whites.white100,
+      shadowOffset: { width: 0, height: -1.5 },
+      shadowOpacity: 0.25,
+      shadowRadius: 1,
+      elevation: 3,
+    },
+    buttonText: {
+      color: "white",
+      fontFamily: typography.bold,
+      fontSize: moderateScale(16),
     },
   });
 };
