63154d2f79c297f5ac7c6d4507193b123753a201
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-20 11:49:58 -0700

    Add LockIcon

---
+++ src/assets/icons/LockIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Svg, { SvgProps, Path } from "react-native-svg";
+
+const SvgComponent = (props: SvgProps) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    className="w-6 h-6"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <Path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25z"
+    />
+  </Svg>
+);
+export default SvgComponent;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -16,6 +16,7 @@ export { default as MessageOption } from "./MessageOption";
 export { default as EditIcon } from "./EditIcon";
 export { default as DeleteIcon } from "./DeleteIcon";
 export { default as BackIcon } from "./BackIcon";
+export { default as LockIcon } from "./LockIcon";
 
 export { default as ErrorIcon } from "./error.png";
 export { default as SuccessIcon } from "./success.png";
