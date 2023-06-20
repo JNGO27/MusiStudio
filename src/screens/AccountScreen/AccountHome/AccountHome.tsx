99605e46ced611d5781998d4d1c342fd068440f8
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-19 19:21:45 -0700

    Add log out functionality to AccountHome with WarningModal message

--- src/screens/AccountScreen/AccountHome/AccountHome.tsx
+++ src/screens/AccountScreen/AccountHome/AccountHome.tsx
@@ -1,19 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import useResponsiveness from "@src/hooks/useResponsiveness";
-import { BackButtonCustom, AccountScreenNavOption } from "@src/components";
+import { supabaseConfig } from "@src/lib/supabaseConfig";
+
+import { useResponsiveness, useNewModalState } from "@src/hooks";
+import {
+  BackButtonCustom,
+  AccountScreenNavOption,
+  WarningModal,
+} from "@src/components";
 
 import { HeaderNavOptionsOnlyArr } from "@src/utils/constants";
 
 import createStyleSheet from "./styles";
 
 const AccountHome = () => {
+  const [modalVisible, openOrCloseModal] = useNewModalState();
+
   const [horizontalScale, verticalScale, moderateScale] = useResponsiveness();
   const styles = createStyleSheet(
     horizontalScale,
     verticalScale,
     moderateScale,
   );
+
+  const handleSignOut = async () => {
+    const { error } = await supabaseConfig.auth.signOut();
+    if (error) return null;
+
+    return null;
+  };
 
   return (
     <View style={styles.screenContainer}>
@@ -29,6 +44,17 @@ const AccountHome = () => {
           />
         ))}
       </View>
+      <TouchableOpacity style={styles.logOutButton} onPress={openOrCloseModal}>
+        <Text style={styles.buttonText}>Log Out</Text>
+      </TouchableOpacity>
+      <WarningModal
+        warningHeaderText="Are you sure?"
+        warningBodyText="Logging out will restrict data to your account. You will need to log back in to access your data."
+        warningActionText="Log Out"
+        modalVisible={modalVisible}
+        openOrCloseModal={openOrCloseModal}
+        dispatchWarningAction={handleSignOut}
+      />
     </View>
   );
 };
