8cdc3276efe6456391aebf4b00a6e31a8d1558f9
Joseph Ochoa <josephochoa91@gmail.com>
2023-06-20 18:25:25 -0700

    Update AccountNavOption to do the same as AccountInformation screen

--- src/components/AccountNavOption/AccountNavOption.tsx
+++ src/components/AccountNavOption/AccountNavOption.tsx
@@ -1,10 +1,14 @@
 import { TouchableOpacity } from "react-native";
+import { Image } from "expo-image";
 import { useNavigation } from "@react-navigation/native";
 
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack/lib/typescript/src/types";
 
+import { useAppSelector } from "@src/redux";
+import { getUserAvatarUrl } from "@src/redux/selectors";
+
 import useResponsiveness from "@src/hooks/useResponsiveness";
-import { UserAccountIcon } from "@src/assets/icons";
+import { MusicNote } from "@src/assets/icons";
 
 import createStyleSheet from "./styles";
 
@@ -15,6 +19,7 @@ type AccountNavigationProps = NativeStackNavigationProp<
 
 const AccountNavOption = () => {
   const navigator = useNavigation<AccountNavigationProps>();
+  const userAvatarUrl = useAppSelector(getUserAvatarUrl);
 
   const [horizontalScale, verticalScale, moderateScale] = useResponsiveness();
   const styles = createStyleSheet(
@@ -31,7 +36,11 @@ const AccountNavOption = () => {
 
   return (
     <TouchableOpacity style={styles.accountNavContainer} onPress={onPress}>
-      <UserAccountIcon style={styles.accountIcon} />
+      {userAvatarUrl ? (
+        <Image source={userAvatarUrl} style={styles.accountIcon} />
+      ) : (
+        <MusicNote style={styles.accountIcon} color="white" />
+      )}
     </TouchableOpacity>
   );
 };
