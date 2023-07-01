7578c2bcd569cc07085b4467252f9e68a7cd0ed5
Joseph Ochoa <josephochoa91@gmail.com>
2023-07-01 15:44:34 -0700

    Add Footer with links

--- web/landing-page/app/page.tsx
+++ web/landing-page/app/page.tsx
@@ -1,32 +1,45 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import { PRIVACY_POLICY_LINK, TERMS_AND_SERVICE_LINK } from "../constants";
 
 import MusiStudioLogo from "../public/icon.png";
 import styles from "./page.module.css";
 
 const Home = () => {
   return (
-    <main className={styles.background}>
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <span />
-      <div className={styles["background-card"]}>
-        <Image
-          className={styles["logo-image"]}
-          src={MusiStudioLogo}
-          width={150}
-          height={150}
-          alt="MusiStudio Logo"
-        />
-        <h1 className={styles["headline-text"]}>MusiStudio</h1>
-      </div>
-    </main>
+    <>
+      <main className={styles.background}>
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <div className={styles["background-card"]}>
+          <Image
+            className={styles["logo-image"]}
+            src={MusiStudioLogo}
+            width={150}
+            height={150}
+            alt="MusiStudio Logo"
+          />
+          <h1 className={styles["headline-text"]}>MusiStudio</h1>
+        </div>
+      </main>
+      <footer>
+        <h3>
+          <Link href={PRIVACY_POLICY_LINK}>Privacy Policy</Link>
+        </h3>
+        <h3>
+          <Link href={TERMS_AND_SERVICE_LINK}>Terms and Service</Link>
+        </h3>
+      </footer>
+    </>
   );
 };
 
---
+++ web/landing-page/constants.ts
@@ -0,0 +1,5 @@
+export const PRIVACY_POLICY_LINK =
+  "https://precious-alyssum-565.notion.site/MusiStudio-Privacy-Policy-593d370323d348c3acc40d77e09bf344?pvs=4";
+
+export const TERMS_AND_SERVICE_LINK =
+  "https://precious-alyssum-565.notion.site/MusiStudio-Terms-and-Service-752e47c9cd1f4b9d96caec49b61a5ed3?pvs=4";
