10683751deb00ce22ccda77acdb12b6aa2523382
Joseph Ochoa <josephochoa91@gmail.com>
2023-07-04 12:55:56 -0700

    Finalize config for production

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,11 @@
 import type { ConfigContext } from "@expo/config";
-import { SUPABASE_URL, SUPABASE_KEY } from "@env";
 
 const expoConfig = ({ config }: ConfigContext) => ({
   ...config,
   name: "MusiStudio",
   slug: "musistudio",
   scheme: "musistudio",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./src/assets/splash/icon.png",
   userInterfaceStyle: "light",
@@ -24,7 +23,7 @@ const expoConfig = ({ config }: ConfigContext) => ({
   },
   android: {
     package: "com.jngo27.musistudio",
-    versionCode: 1,
+    versionCode: 1.1,
   },
   web: {
     favicon: "./src/assets/favicon.png",
@@ -32,8 +31,6 @@ const expoConfig = ({ config }: ConfigContext) => ({
   extra: {
     eas: {
       projectId: "4554bb24-6fed-4647-ab29-99da32903180",
-      SUPABASE_URL,
-      SUPABASE_KEY,
     },
   },
   owner: "jngo27",
--- src/lib/supabaseConfig.ts
+++ src/lib/supabaseConfig.ts
@@ -2,8 +2,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import "react-native-url-polyfill/auto";
 
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_KEY } from "@env";
 
+// Safe to use in client due to Supabase safety
+const SUPABASE_URL = "https://fkxmnihdzrodgtmebjfs.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZreG1uaWhkenJvZGd0bWViamZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1MzM4MzIsImV4cCI6MTk5MzEwOTgzMn0.AcAC3br3ZTk77GNZz-XHSLwfb_hPuSRxW5JBzXvX1rk";
 // eslint-disable-next-line import/prefer-default-export
 export const supabaseConfig = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
